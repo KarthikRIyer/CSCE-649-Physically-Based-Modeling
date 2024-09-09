bbe30f1c68ed62bcafe3f900fca5093818809a7e
Karthik Iyer <Karthik.iyer2@yandex.com>
2024-09-09 14:40:11 -0500

    add wind

--- A1/src/Scene.cpp
+++ A1/src/Scene.cpp
@@ -76,7 +76,7 @@ void Scene::load(const string &RESOURCE_DIR, const string &DATA_DIR, int texUnit
 	gravity = std::make_shared<Gravity>(Eigen::Vector3d(0.0, -9.8, 0.0));
 	wind = std::make_shared<Wind>(5.0, Eigen::Vector3d(1.0, 0.0, 0.0));
 	forceFields.push_back(gravity);
-//	forceFields.push_back(wind);
+	forceFields.push_back(wind);
 
 	if (sceneIndex == 0) {
         sphereShape = make_shared<Shape>();
@@ -277,8 +277,8 @@ void Scene::step()
     t += cf * h;
 
     // update wind
-    Eigen::Vector3d wP(0.0, 0.0, -5.0);
-    Eigen::Vector3d wDir(0.0, 0.0, -5.0);
+    Eigen::Vector3d wP(0.0, 0.0, 0.0);
+    Eigen::Vector3d wDir(0.0, 0.0, 0.0);
     wDir.x() = 5.0 * sin(simParams.windOscilationSpeed * t);
     wind->setDirection(wDir);
     wind->setStrength(simParams.windStrength);
--- A1/src/main.cpp
+++ A1/src/main.cpp
@@ -210,8 +210,8 @@ void render()
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
     ImGui::Begin("Simulation params");
-//    ImGui::SliderFloat("Wind strength", &simParams.windStrength, 0.0f, 10.0f);
-//    ImGui::SliderFloat("Wind oscillation speed", &simParams.windOscilationSpeed, 0.0f, 10.0f);
+    ImGui::SliderFloat("Wind strength", &simParams.windStrength, 0.0f, 10.0f);
+    ImGui::SliderFloat("Wind oscillation speed", &simParams.windOscilationSpeed, 0.0f, 10.0f);
     ImGui::SliderFloat("Air Friction Factor", &simParams.airFrictionFactor, 0.0f, 5.0f);
     ImGui::SliderFloat("Restitution Coefficient", &simParams.restitutionCoeff, 0.0f, 1.0f);
     ImGui::SliderFloat("Friction Coefficient", &simParams.frictionCoeff, 0.0f, 5.0f);
