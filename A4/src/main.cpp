efe854525261ccf9e1276ce658e1b64165b3aaa5
Karthik Iyer <Karthik.iyer2@yandex.com>
2024-10-29 22:30:15 -0500

    change window title

--- A4/src/main.cpp
+++ A4/src/main.cpp
@@ -345,7 +345,7 @@ int main(int argc, char **argv)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	// Create a windowed mode window and its OpenGL context.
-	window = glfwCreateWindow(640*2, 480*2, "A2 - Particle Systems", NULL, NULL);
+	window = glfwCreateWindow(640*2, 480*2, "A4 - Springy Structures", NULL, NULL);
 	if(!window) {
 		glfwTerminate();
 		return -1;
